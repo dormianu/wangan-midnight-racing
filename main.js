42474a2867236a0d0aedce88d727c6198aa9f007
dormianu <fsndr38@gmail.com>
2026-06-10 10:23:46 -0700

    Add main entry point

---
+++ main.js
@@ -0,0 +1,8 @@
+import * as THREE from 'three';
+import { Car } from './src/Car.js';
+import { Track } from './src/Track.js';
+import { Game } from './src/Game.js';
+
+// Initialize game
+const game = new Game();
+game.init();
